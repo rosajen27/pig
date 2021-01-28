3ab5b1d978782b05a62ba3133d583a668becd863
Jennifer Rosa <rosajen27@gmail.com>
2021-01-27 20:08:37 -0500

    roll dice functionality

--- script.js
+++ script.js
@@ -3,9 +3,38 @@
 // selecting elements
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
 const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+let currentScore = 0;
+
+// rolling the dice
+btnRoll.addEventListener("click", function () {
+    // generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // display the dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`
+
+    // check for a rolled 1 
+    if (dice !== 1) {
+        // add dice to current score
+        currentScore += dice;
+        current0El.textContent = currentScore; // CHANGE LATER TO ADD SCORE TO CURRENT PLAYER
+
+    } else {
+        // switch to next player
+
+    }
+})
